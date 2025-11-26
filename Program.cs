692d1a868d59255439099503432e30501c84db2e
Муса <good.24165@gmail.com>
2025-11-26 14:36:13 +0300

    Final programm

--- Program.cs
+++ Program.cs
@@ -1,62 +1,34 @@
 ﻿using System;
-using System.Numerics;
-using System.Reflection;
-using System.Collections;
+using System.Collections.Generic;
 
 namespace MyApp
 {
-    public class Vertex 
+    public static class BfsIterator
     {
-        private readonly List<Vertex> neighbours;
-        public IEnumerable<Vertex> Neighbors => neighbours;
-        public int id;
-        
-        public Vertex(int id)
+        public static IEnumerable<int> Bfs(Graph g, int start)
         {
-            this.id = id;
-            neighbours = new List<Vertex>();
-        }
-        public List<Vertex> GetNeighboursList() => neighbours;
-        public void AddNeighbour(Vertex id) => neighbours.Add(id);
-        public void RemoveNeighbour(Vertex id) => neighbours.Remove(id);
-        public Vertex NeighborAt(int index)
-        {
-            if (index < 0 || index >= neighbours.Count) return null;
-            return neighbours[index];
-        }
-        public bool HasNeighbor(int id) => neighbours.Any(n => n.id == id);
-        public void SeeNeighbors()
-        {
-            Console.Write($"Vertex {id} neighbors: ");
-            foreach (var neighbor in neighbours)
+            if (start < 0 || start >= g.Count) yield break;
+
+            var seen = new bool[g.Count];
+            var q = new Queue<int>();
+
+            q.Enqueue(start);
+            seen[start] = true;
+
+            while (q.Count > 0)
             {
-                Console.Write($"{neighbor.id} ");
-            }
-            Console.WriteLine();
-        }
-    }
-    public class BfsIterator 
-    {
-        private BfsIterator() { }
-        public static IEnumerable<Vertex> Bfs(Graph graph, int startId)
-        {
-            if (graph.Vertex(startId) == null) yield break;
-            Queue<Vertex> _queue = new Queue<Vertex>();
-            HashSet<int> _seen = new HashSet<int>();
+                int v = q.Dequeue();
 
-            _queue.Enqueue(graph.Vertex(startId));
-            _seen.Add(graph.Vertex(startId).id);
+                yield return v;
 
-            while (_queue.Count > 0)
-            {
-                Vertex currentVertex = _queue.Dequeue();
-                yield return currentVertex;
-
-                foreach (Vertex neighbour in currentVertex.Neighbors)
+                for (int u = g.First(v); u != -1; u = g.Next(v, u))
                 {
-                    if (_seen.Contains(neighbour.id)) continue;
-                    _seen.Add(neighbour.id);
-                    _queue.Enqueue(neighbour);
+                    if (u < 0 || u >= g.Count) continue;
+                    if (!seen[u])
+                    {
+                        seen[u] = true;
+                        q.Enqueue(u);
+                    }
                 }
             }
         }
@@ -64,167 +36,277 @@ namespace MyApp
 
     public class Graph
     {
-        private int countOfVertices;
-        private Dictionary<int, Vertex> vertices;
         private List<List<int>> adjacencyMatrix;
+        public int Count => adjacencyMatrix.Count;
 
         public Graph()
         {
-            countOfVertices = 0;
-            vertices = new Dictionary<int, Vertex>();
+            adjacencyMatrix = new List<List<int>>();
         }
-        public Vertex First(int vertexId)
-        {
-            if (!vertices.ContainsKey(vertexId)) return null;
-            if (vertices[vertexId].NeighborAt(0) != null) return vertices[vertexId].NeighborAt(0);
-            else return null;
-        }
-        public Vertex Next(int vertexId, int i)
-        {
-            if (vertices[vertexId].NeighborAt(i) != null) return vertices[vertexId].NeighborAt(i);
-            else return null;
-        }
-        public Vertex Vertex(int vertexId)
-        {
-            if (!vertices.ContainsKey(vertexId)) return null;
-            return vertices[vertexId];
-        }
-        public void Add_V(int id) 
-        {
-            vertices.TryAdd(id, new Vertex(id));
-        }
-        public void Add_E(int fromId, int toId) 
-        {
-            Vertex vertexFrom = vertices[fromId];
-            Vertex vertexTo = vertices[toId];
-            if (!vertexFrom.HasNeighbor(toId)) vertexFrom.AddNeighbour(vertices[toId]);
-            if (!vertexTo.HasNeighbor(fromId)) vertexTo.AddNeighbour(vertices[fromId]);
-        }
-        public void Del_V(int id) 
-        {            
-            foreach (var vertex in vertices.Values)
-            {
-                vertex.RemoveNeighbour(vertices[id]);
-            }
-            vertices.Remove(id);
-        }
-        public void Del_E(int fromId, int toId) 
-        {
-            Vertex vertexFrom = vertices[fromId];
-            Vertex vertexTo = vertices[toId];
 
-            if (vertexFrom.HasNeighbor(toId)) vertexFrom.RemoveNeighbour(vertexTo);
-            if (vertexTo.HasNeighbor(fromId)) vertexTo.RemoveNeighbour(vertexFrom);
-        }
-        public void Edit_V(int id, int newId) 
-        {            
-            if (vertices.ContainsKey(id))
-            {
-                Vertex vertex = vertices[id];
-                vertices.Remove(id);
-                vertex.id = newId;
-                vertices.Add(newId, vertex);
-            }
-        }
-        public void CreateAdjencencyMatrix()
+        public int First(int v)
         {
-            int n = vertices.Count;
-            adjacencyMatrix = new List<List<int>>(n);
-            for (int i = 0; i < n; i++)
+            if (v < 0 || v >= adjacencyMatrix.Count) return -1;
+            for (int j = 0; j < adjacencyMatrix.Count; j++)
             {
-                adjacencyMatrix.Add(new List<int>(new int[n]));
+                if (adjacencyMatrix[v][j] == 1) return j;
             }
-            var vertexIds = new List<int>(vertices.Keys);
-            for (int i = 0; i < vertexIds.Count; i++)
+            return -1;
+        }
+
+        public int Next(int v, int i)
+        {
+            if (v < 0 || v >= adjacencyMatrix.Count) return -1;
+            for (int j = i + 1; j < adjacencyMatrix.Count; j++)
+                if (adjacencyMatrix[v][j] == 1) return j;
+            return -1;
+        }
+
+        public int Vertex(int v, int k)
+        {
+            if (v < 0 || v >= adjacencyMatrix.Count) return -1;
+            int c = 0;
+            for (int j = 0; j < adjacencyMatrix.Count; j++)
             {
-                var vertex = vertices[vertexIds[i]];
-                for (int j = 0; j < vertexIds.Count; j++)
+                if (adjacencyMatrix[v][j] == 1)
                 {
-                    if (vertex.HasNeighbor(vertexIds[j]))
-                    {
-                        adjacencyMatrix[i][j] = 1;
-                    }
-                    else
-                    {
-                        adjacencyMatrix[i][j] = 0;
-                    }
+                    if (c == k) return j;
+                    c++;
                 }
             }
+            return -1;
         }
+
+        public void Add_V()
+        {
+            for (int i = 0; i < adjacencyMatrix.Count; i++)
+            {
+                adjacencyMatrix[i].Add(0);
+            }
+            var list = new List<int>(adjacencyMatrix.Count + 1);
+            for (int i = 0; i < adjacencyMatrix.Count + 1; i++)
+            {
+                list.Add(0);
+            }
+            adjacencyMatrix.Add(list);
+        }
+
+        public void Add_E(int fromId, int toId)
+        {
+            if (fromId < 0 || toId < 0) return;
+            if (fromId >= adjacencyMatrix.Count) return;
+            if (toId >= adjacencyMatrix[fromId].Count) return;
+
+            adjacencyMatrix[fromId][toId] = 1;
+            adjacencyMatrix[toId][fromId] = 1;
+        }
+
+        public void Del_V(int id)
+        {
+            if (id < 0 || id >= adjacencyMatrix.Count) return;
+
+            for (int i = 0; i < adjacencyMatrix.Count; i++)
+            {
+                adjacencyMatrix[i].RemoveAt(id);
+            }
+
+            adjacencyMatrix.RemoveAt(id);
+        }
+
+        public void Del_E(int fromId, int toId)
+        {
+            if (fromId < 0 || toId < 0) return;
+            if (fromId >= adjacencyMatrix.Count) return;
+            if (toId >= adjacencyMatrix[fromId].Count) return;
+
+            adjacencyMatrix[fromId][toId] = 0;
+            adjacencyMatrix[toId][fromId] = 0;
+        }
+
+        public void Edit_V(int id, int newId)
+        {
+            if (id < 0 || newId < 0) return;
+            if (id >= adjacencyMatrix.Count || newId >= adjacencyMatrix.Count) return;
+            if (id == newId) return;
+
+            var tmpRow = adjacencyMatrix[id];
+            adjacencyMatrix[id] = adjacencyMatrix[newId];
+            adjacencyMatrix[newId] = tmpRow;
+
+            for (int i = 0; i < adjacencyMatrix.Count; i++)
+            {
+                int tmp = adjacencyMatrix[i][id];
+                adjacencyMatrix[i][id] = adjacencyMatrix[i][newId];
+                adjacencyMatrix[i][newId] = tmp;
+            }
+        }
+
         public void PrintAdjacencyMatrix()
         {
-            if (adjacencyMatrix == null || adjacencyMatrix.Count == 0)
-            {
-                Console.WriteLine("Adjacency matrix is empty. Call CreateAdjencencyMatrix() first.");
-                return;
-            }
-
-            var vertexIds = new List<int>(vertices.Keys);
-            int n = adjacencyMatrix.Count;
-
-            if (vertexIds.Count != n)
-            {
-                Console.WriteLine("Warning: vertex set changed after building matrix.");
-                vertexIds = Enumerable.Range(0, n).ToList();
-            }
-
-            // шапка
             Console.Write("    ");
-            for (int j = 0; j < n; j++) Console.Write($"{vertexIds[j],3}");
-            Console.WriteLine();
-
-            Console.Write("    ");
-            for (int j = 0; j < n; j++) Console.Write("---");
-            Console.WriteLine();
-
-            // строки
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < adjacencyMatrix.Count; i++)
             {
-                Console.Write($"{vertexIds[i],3}|");
-                for (int j = 0; j < n; j++)
-                    Console.Write($"{adjacencyMatrix[i][j],3}");
+                Console.Write((i) + " ");
+            }
+            Console.WriteLine();
+            int j = 0;
+            foreach (var vertex in adjacencyMatrix)
+            {
+                Console.Write(j + " | ");
+                j++;
+                if (vertex.Count > 0)
+                {
+                    foreach (var edge in vertex)
+                    {
+                        Console.Write(edge + " ");
+                    }
+                }
                 Console.WriteLine();
             }
         }
-        public void Debug()
+    }
+    public static class CycleCounterBfs
+    {
+        public static List<List<int>> FindAllCycles(Graph g)
         {
-            foreach (var vertex in vertices)
+            int n = g.Count;
+            var cyclesSet = new HashSet<string>();
+            var cycles = new List<List<int>>();
+
+            for (int start = 0; start < n; start++)
             {
-                Console.Write(vertex.Key + "-> ");
-                vertex.Value.SeeNeighbors();
+                var q = new Queue<List<int>>();
+                q.Enqueue(new List<int> { start });
+
+                while (q.Count > 0)
+                {
+                    var path = q.Dequeue();
+                    int v = path[path.Count - 1]; 
+
+                    for (int u = g.First(v); u != -1; u = g.Next(v, u))
+                    {
+                        if (u == start && path.Count >= 3)
+                        {
+                            AddCycle(path, cyclesSet, cycles);
+                        }
+                        else if (!path.Contains(u))
+                        {
+                            var newPath = new List<int>(path);
+                            newPath.Add(u);
+                            q.Enqueue(newPath);
+                        }
+                    }
+                }
+            }
+
+            return cycles;
+        }
+
+        private static void AddCycle(List<int> path, HashSet<string> cyclesSet, List<List<int>> cycles)
+        {
+            int k = path.Count;
+            var forward = path.ToArray();
+
+            var backward = new int[k];
+            for (int i = 0; i < k; i++)
+                backward[i] = forward[k - 1 - i];
+
+            var canonForward = MakeMinFirst(forward);
+            var canonBackward = MakeMinFirst(backward);
+
+            string keyForward = string.Join("-", canonForward);
+            string keyBackward = string.Join("-", canonBackward);
+
+            string key = string.CompareOrdinal(keyForward, keyBackward) <= 0
+                ? keyForward
+                : keyBackward;
+
+            if (cyclesSet.Add(key))
+            {
+                cycles.Add(new List<int>(canonForward));
             }
         }
+
+        private static int[] MakeMinFirst(int[] cycle)
+        {
+            int k = cycle.Length;
+            int min = cycle[0];
+            int minIndex = 0;
+
+            for (int i = 1; i < k; i++)
+            {
+                if (cycle[i] < min)
+                {
+                    min = cycle[i];
+                    minIndex = i;
+                }
+            }
+
+            var res = new int[k];
+            for (int i = 0; i < k; i++)
+            {
+                res[i] = cycle[(i + minIndex) % k];
+            }
+            return res;
+        }
     }
-    
 
     internal class Program
     {
         static void Main(string[] args)
         {
             var graf = new Graph();
-            graf.Add_V(1);
-            graf.Add_V(2);
-            graf.Add_V(3);
-            graf.Add_V(4);
+            graf.Add_V();
+            graf.Add_V();
+            graf.Add_V();
+            graf.Add_V();
+            graf.Add_V();
+            graf.Add_V();
 
-            graf.Add_E(1, 2);
+            graf.Add_E(0, 1);
+            graf.Add_E(0, 2);
+            graf.Add_E(0, 3);
             graf.Add_E(1, 3);
-            graf.Add_E(3, 2);
             graf.Add_E(2, 4);
             graf.Add_E(3, 4);
+            graf.Add_E(3, 5);
+            graf.Add_E(4, 5);
 
-            foreach(Vertex v in BfsIterator.Bfs(graf, 1))
+            Console.WriteLine("Матрица смежности:");
+            graf.PrintAdjacencyMatrix();
+            Console.WriteLine("\n-----------------------------------------");
+            Console.WriteLine("Обход графа в ширину (Вывод 1):");
+            foreach (var v in BfsIterator.Bfs(graf, 0))
             {
-                Console.WriteLine(v.id);
+                Console.Write(v + " ");
             }
+            Console.WriteLine("\n-----------------------------------------");
+            Console.WriteLine("Обход графа в ширину (Вывод 2):");
+            foreach (var v in BfsIterator.Bfs(graf, 1))
+            {
+                Console.Write(v + " ");
+            }
+            Console.WriteLine("\n-----------------------------------------");
+            Console.WriteLine("Обход графа в ширину (Вывод 3):");
+            foreach (var v in BfsIterator.Bfs(graf, 3))
+            {
+                Console.Write(v + " ");
+            }
+            Console.WriteLine("\n-----------------------------------------");
 
-            //graf.Debug();
-            //graf.CreateAdjencencyMatrix();
-            //graf.PrintAdjacencyMatrix();
-
-            //Console.WriteLine(graf.Next(1, 2).id);
-            //Console.WriteLine(graf.First(1).id);
-            //Console.WriteLine(graf.First(2).id);
+            var cycles = CycleCounterBfs.FindAllCycles(graf);
+            Console.WriteLine("Все циклы:");
+            foreach (var cycle in cycles)
+            {
+                for (int i = 0; i < cycle.Count; i++)
+                {
+                    Console.Write(cycle[i] + " ");
+                }
+                Console.Write(cycle[0]);
+                Console.WriteLine();
+            }
+            Console.WriteLine("Общее количество циклов: " + cycles.Count);
+            Console.ReadLine();
         }
     }
 }
--- obj/Debug/net8.0/Grafs.AssemblyInfo.cs
+++ obj/Debug/net8.0/Grafs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Grafs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57e145935a954aff95b3a5f8c6d996478c01c779")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df3528c6c82efa9c95272dc111fd6416cf0711f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Grafs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Grafs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
